98a22c5cd89e70f09b38fa684868347ef750e42b
Amperka04 <49093447+Amperka04@users.noreply.github.com>
2025-05-22 22:57:34 +0300

    Add files via upload

---
+++ hello_world.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+//int main(){				//start of program
+//	string name;			//creating string name переменной имени
+//	std::cout << "Enter Name: \n";	//вывод запроса о вводе name
+//	std::cin >> name; 		//input name
+//	std::cout << "Hello worlD " << name << "!";	//вывод имени
+//}
+
+int main()
+{              // начало программы
+  std::string name; // создание переменной имени
+  std::cout << "Enter Name: \n"; // вывод запроса о вводе имени
+  std::cin >> name;              // ввод имени
+  std::cout << "Hello World " << name << "!!11!!!"; // вывод имени
+}
